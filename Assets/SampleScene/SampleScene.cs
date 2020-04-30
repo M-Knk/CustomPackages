7b5304aa44930f1a02146a096f9c0e72cd5266e9
M-Knk <foo@bar.com>
2020-04-30 11:29:06 +0900

    add test script

---
+++ Assets/SampleScene/SampleScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomPackage.Test;
+
+public class SampleScene : MonoBehaviour
+{
+    void Start()
+    {
+		RuntimeTest.Log();
+    }
+}
---
+++ Assets/Test/Runtime/RuntimeTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomPackage.Test
+{
+	public static class RuntimeTest
+	{
+		public static void Log()
+		{
+			Debug.LogError("This is Test.");
+		}
+	}
+}
