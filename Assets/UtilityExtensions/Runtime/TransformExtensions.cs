de28371d6dc5d41be22eb2c7ea5ab65c61f2d87d
M-Knk <foo@bar.com>
2020-05-07 21:15:41 +0900

    Utility Extensions

---
+++ Assets/UtilityExtensions/Runtime/TransformExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomPackage.UtililtyExtensions
+{
+	public static class TransformExtensions
+	{
+		public static void Initiailze(this Transform transform)
+		{
+			transform.localPosition = Vector3.zero;
+			transform.localRotation = Quaternion.identity;
+			transform.localScale = Vector3.one;
+		}
+	}
+}
